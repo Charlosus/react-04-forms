0b671a99e3d1ad790fb75fc162e0e072784a33bf
Charlosus <145377783+Charlosus@users.noreply.github.com>
2025-06-08 09:53:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
 import './App.css';
 
 // when using form we dont use onclick we use onSubmit on form and this will submit form and use event
